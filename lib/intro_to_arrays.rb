36173850d1971d013ae8d48148292d898dac0c70
Min Hee  Park <minheekangg@gmail.com>
2018-09-22 20:42:09 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+array = []
+end
+
+def array_with_two_elements
+  ["a","b"]
+end
+
+def first_element(arr)
+  arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  arr.shift
+end
+
+def last_element_with_array_methods(arr)
+  arr.pop
+end
+
+def length_of_array(arr)
+  arr.length
+end
