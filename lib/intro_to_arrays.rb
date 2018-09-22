070a3c99cb76e25f79abf8b4d78625369f6aa141
Min Hee  Park <minheekangg@gmail.com>
2018-09-22 20:41:34 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -13,3 +13,19 @@ end
 def third_element(arr)
   arr[2]
 end
+
+def last_element(arr)
+  arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  arr.shift
+end
+
+def last_element_with_array_methods(arr)
+  arr.pop
+end
+
+def length_of_array
+  arr.length
+end
